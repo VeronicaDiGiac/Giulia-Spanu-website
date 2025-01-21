280734e0e9aab458c1586df7102c1492f2cdc48f
veronica <veronicadigiacomo190@gmail.com>
2025-01-21 18:12:35 +0100

    aggiornata validazione lato form
    Signed-off-by: veronica <veronicadigiacomo190@gmail.com>

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 import {
   FormControl,
   FormGroup,
@@ -9,7 +10,7 @@ import emailjs from '@emailjs/browser';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf],
   template: `<section class="p-4 sm:p-6 md:p-8">
     <div class="max-w-lg mx-auto bg-gray-200 p-4 sm:p-6 rounded-lg shadow-lg">
       <form [formGroup]="contactForm" (ngSubmit)="onSubmit()">
@@ -25,6 +26,16 @@ import emailjs from '@emailjs/browser';
               class="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300"
               [class.border-red-500]="isFieldInvalid('nome')"
             />
+            <small
+              class="text-red-500"
+              *ngIf="isFieldInvalid('nome') && contactForm.get('nome')?.errors?.['required']"
+              >Il campo Nome è obbligatorio.</small
+            >
+            <small
+              class="text-red-500"
+              *ngIf="isFieldInvalid('nome') && contactForm.get('nome')?.errors?. ['minlength']"
+              >Il Nome deve essere di almeno 3 caratteri.</small
+            >
           </div>
           <div class="mb-4">
             <label for="cognome" class="block text-gray-700 font-medium mb-2"
@@ -37,6 +48,16 @@ import emailjs from '@emailjs/browser';
               class="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300"
               [class.border-red-500]="isFieldInvalid('cognome')"
             />
+            <small
+              class="text-red-500"
+              *ngIf=" isFieldInvalid('cognome') && contactForm.get('cognome')?.errors?.['required']"
+              >Il campo Cognome è obbligatorio.</small
+            >
+            <small
+              class="text-red-500"
+              *ngIf=" isFieldInvalid('cognome') && contactForm.get('cognome')?.errors?.['minlength']"
+              >Il Cognome deve essere di almeno 3 caratteri.</small
+            >
           </div>
           <div class="mb-4 col-span-1 sm:col-span-2">
             <label for="email" class="block text-gray-700 font-medium mb-2"
@@ -49,6 +70,16 @@ import emailjs from '@emailjs/browser';
               class="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300"
               [class.border-red-500]="isFieldInvalid('email')"
             />
+            <small
+              class="text-red-500"
+              *ngIf=" isFieldInvalid('email') && contactForm.get('email')?.errors?. ['required']"
+              >Il campo Email è obbligatorio.</small
+            >
+            <small
+              class="text-red-500"
+              *ngIf="isFieldInvalid('email') && contactForm.get('email')?.errors?.['pattern']"
+              >Inserisci un indirizzo email valido.</small
+            >
           </div>
           <div class="mb-6 col-span-1 sm:col-span-2">
             <label for="messaggio" class="block text-gray-700 font-medium mb-2"
@@ -61,6 +92,21 @@ import emailjs from '@emailjs/browser';
               class="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300"
               [class.border-red-500]="isFieldInvalid('messaggio')"
             ></textarea>
+            <small
+              class="text-red-500"
+              *ngIf="isFieldInvalid('messaggio') && contactForm.get('messaggio')?.errors?.['required']"
+              >Il campo Messaggio è obbligatorio.</small
+            >
+            <small
+              class="text-red-500"
+              *ngIf="isFieldInvalid('messaggio') && contactForm.get('messaggio')?.errors?.['maxlength']"
+              >Il Messaggio non deve superare i 2000 caratteri.</small
+            >
+            <small
+              class="text-red-500"
+              *ngIf=" isFieldInvalid('messaggio') && contactForm.get('messaggio')?.errors?.['minlength']"
+              >Il Messaggio deve essere di almeno 10 caratteri.</small
+            >
           </div>
         </div>
         <div class="text-center">
@@ -73,8 +119,15 @@ import emailjs from '@emailjs/browser';
           </button>
         </div>
       </form>
+
+      <div
+        *ngIf="messageSent"
+        class="mt-4 p-4 text-green-700 bg-green-200 rounded-lg text-center"
+      >
+        Messaggio inviato con successo!
+      </div>
     </div>
-  </section> `,
+  </section>`,
   styles: ``,
 })
 export class ContactComponent {
@@ -90,35 +143,36 @@ export class ContactComponent {
     ]),
     messaggio: new FormControl('', [
       Validators.required,
-      Validators.maxLength(100),
-      Validators.minLength(3),
+      Validators.maxLength(2000),
+      Validators.minLength(10),
     ]),
   });
+
+  messageSent = false;
 
   isFieldInvalid(field: string): boolean {
     const control = this.contactForm.get(field);
     return !!control?.invalid && (control.dirty || control.touched);
   }
+
   onSubmit() {
     if (this.contactForm.valid) {
       const formData = this.contactForm.value;
-      console.log('submit chiamata');
-      console.log(formData.messaggio);
       emailjs
         .send(
           'service_t672znt', // Sostituisci con il tuo ID servizio EmailJS
           'template_6j5v2k4', // Sostituisci con il tuo ID template EmailJS
           {
             nome: `${formData.nome}`,
-            cognome: ` ${formData.cognome}`,
-            email: ` ${formData.email}`,
-            messaggio: ` ${formData.messaggio}`,
+            cognome: `${formData.cognome}`,
+            email: `${formData.email}`,
+            messaggio: `${formData.messaggio}`,
           },
           'wMLjHnKhLQgum55-S' // Sostituisci con la tua chiave pubblica EmailJS
         )
         .then(
           () => {
-            alert('Messaggio inviato con successo!');
+            this.messageSent = true;
             this.contactForm.reset();
           },
           (error) => {
