ad74f473e3541327cd03f4cee57ab062f3bffd20
veronica <veronicadigiacomo190@gmail.com>
2025-01-22 11:14:11 +0100

    Aggiunti dettagli testo per Responsive
    Signed-off-by: veronica <veronicadigiacomo190@gmail.com>

--- src/app/components/chi-sono-section/chi-sono-section.component.ts
+++ src/app/components/chi-sono-section/chi-sono-section.component.ts
@@ -14,8 +14,8 @@ import { Component } from '@angular/core';
           class="w-64 h-64 object-cover rounded-full border-40 foto_profilo"
         />
         <div class="text-center md:text-left max-w-prose">
-          <h2 class="text-2xl font-bold mb-4 text-blue-400">Chi Sono</h2>
-          <p class="text-gray-700 leading-relaxed">
+          <h2 class="text-2xl font-bold mb-10 text-blue-400">Chi Sono</h2>
+          <p class="text-gray-700 leading-relaxed p-5">
             Mi chiamo Giulia Spanu, sono nata nel 1993 a Roma, una città che mi
             ha cresciuta e ispirata con la sua bellezza e il suo patrimonio
             culturale. Sono figlia di un musicista e psicoterapeuta e di
@@ -25,7 +25,7 @@ import { Component } from '@angular/core';
             mio cammino, portandomi a sviluppare una profonda sensibilità verso
             l'interiorità dell'essere umano.
           </p>
-          <p class="text-gray-700 leading-relaxed mt-4">
+          <p class="text-gray-700 leading-relaxed mt-4 p-5">
             Fin da bambina mi sentivo attratta da ciò che si cela dietro i
             pensieri, le emozioni e le azioni delle persone. Allo stesso tempo,
             amavo esplorare la mia creatività attraverso l'arte, che consideravo
@@ -40,7 +40,7 @@ import { Component } from '@angular/core';
       >
         <img class="w-64 h-64 object-cover rounded-full foto_yoga " />
         <div class="text-center md:text-left max-w-prose">
-          <p class="text-gray-700 leading-relaxed">
+          <p class="text-gray-700 leading-relaxed p-5">
             A 14 anni ho incontrato il Kundalini Yoga, un’esperienza che ha
             cambiato la mia vita. Mi sono immersa completamente in questa
             disciplina, trovando in essa non solo un potente strumento di
@@ -50,7 +50,7 @@ import { Component } from '@angular/core';
             un’associazione culturale, di cui sono stata Presidente dal 2014 al
             2019.
           </p>
-          <p class="text-gray-700 leading-relaxed mt-4">
+          <p class="text-gray-700 leading-relaxed mt-4 p-5">
             Parallelamente, ho intrapreso gli studi universitari in Psicologia,
             un percorso che sentivo come una naturale estensione del mio
             interesse per l’animo umano. Ho conseguito la laurea nel 2020, dopo
@@ -59,7 +59,7 @@ import { Component } from '@angular/core';
             psicodramma. Nel 2023 ho superato l’Esame di Stato presso
             l’Università di Pavia, diventando ufficialmente Psicologa.
           </p>
-          <p class="text-gray-700 leading-relaxed mt-4">
+          <p class="text-gray-700 leading-relaxed mt-4 p-5">
             Sin da subito ho lavorato con dedizione, sia in ambito privato che
             scolastico. L’esperienza a contatto con studenti, molti dei quali
             con DSA e ADHD, mi ha insegnato quanto sia importante offrire un
@@ -73,7 +73,7 @@ import { Component } from '@angular/core';
       >
         <img class="w-64 h-64 object-cover   foto_arte" />
         <div class="text-center md:text-left max-w-prose">
-          <p class="text-gray-700 leading-relaxed">
+          <p class="text-gray-700 leading-relaxed p-5">
             Oggi sono specializzanda in Psicoterapia Umanistico Esistenziale, un
             approccio che mette al centro l’individuo nella sua unicità,
             valorizzandone risorse e potenzialità. Il mio lavoro come Psicologa
@@ -84,13 +84,13 @@ import { Component } from '@angular/core';
             miei pazienti a riconnettersi con il proprio corpo e a gestire
             meglio lo stress.
           </p>
-          <p class="text-gray-700 leading-relaxed mt-4">
+          <p class="text-gray-700 leading-relaxed mt-4 p-5">
             Credo profondamente che ogni persona abbia in sé la capacità di
             trasformare le proprie ferite in forza e creatività. Il mio compito
             è accompagnare chi si affida a me in questo viaggio di scoperta,
             offrendogli uno spazio sicuro dove sentirsi accolto e compreso.
           </p>
-          <p class="text-gray-700 leading-relaxed mt-4 ">
+          <p class="text-gray-700 leading-relaxed mt-4 p-5">
             Se vuoi conoscermi meglio o iniziare un percorso insieme, sarò
             felice di incontrarti.
           </p>
